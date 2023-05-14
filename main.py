829017d6180827b872e0c84b5cbe0293bb352305
siddharth Tyagi <sidtyagi05@gmail.com>
2023-05-14 23:05:23 +0530

    project structure added

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import os  
+from sensor.utils.main_utils import read_yaml_file
+
+
+env_file_path=os.path.join(os.getcwd(),"env.yaml")
+
+def set_env_variable(env_file_path):
+
+    if os.getenv('MONGO_DB_URL',None) is None:
+        env_config = read_yaml_file(env_file_path)
+        os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
+        print("Environemnt variable set")
+
