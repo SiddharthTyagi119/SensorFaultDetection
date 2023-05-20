05cf74027dcf432d7764f95701f7a5e623cea708
siddharth Tyagi <sidtyagi05@gmail.com>
2023-05-20 21:25:45 +0530

    trainng evaluation done

---
+++ sensor/cloud storage/s3_syncer.py
@@ -0,0 +1,13 @@
+from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME, PREDICTION_BUCKET_NAME, AWS_BUCKET_URL
+import os
+class S3Sync:
+
+
+    def sync_folder_to_s3(self,folder,AWS_BUCKET_URL):
+        command = f"aws s3 sync {folder} {AWS_BUCKET_URL} "
+        os.system(command)
+
+    def sync_folder_from_s3(self,folder,AWS_BUCKET_URL):
+        command = f"aws s3 sync  {AWS_BUCKET_URL} {folder} "
+        os.system(command)
+
